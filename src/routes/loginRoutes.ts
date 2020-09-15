0ce2409a373d1ca6b6b6717f3f6ea3760759f3e4
bastej <siatkasebastian@gmail.com>
2020-09-15 21:42:12 +0200

    Checking user login status

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -25,10 +25,24 @@ router.get("/login", (req: Request, res: Response) => {
 router.post("/login", (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
 
-  if (email && password) {
-    res.send(email + password);
+  if (email && password && email === "a@b.c" && password === "test") {
+    req.session = { loggedIn: true };
+    res.redirect("/");
   } else {
-    res.send("You must provide both an email and a password");
+    res.send("Invalid email or password");
+  }
+});
+
+router.get("/", (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+            <div>
+                <div>You are logged in</div>
+                <a href="/logout">Logout</a>
+            </div>
+        `);
+  } else {
+    res.send("You are not logged in");
   }
 });
 
